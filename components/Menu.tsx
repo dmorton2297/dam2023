a3eefb083ad8cb6badfe2285b6388350119625bb
Dan Morton <dmorton@project44.com>
2022-12-18 15:48:40 -0500

    Test

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,33 +1,40 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { slide as Slide } from "react-burger-menu";
+import { Seperator } from "./core/Seperator";
 
 export const Menu: React.FC = () => {
   return (
     <Slide width={300} pageWrapId="page-wrap" outerContainerId="outer">
-      <Link id="home" className="p-8 menu-item" href="/">
+      <Link id="home" className="p-8 menu-item font-serif" href="/">
         Home
       </Link>
-      <Link id="home" className="p-8 menu-item" href="/event">
+      <Seperator />
+      <Link id="home" className="p-8 menu-item font-serif" href="/event">
         Wedding
       </Link>
-      <Link id="home" className="p-8 menu-item" href="/event">
+      <Seperator />
+
+      <Link id="home" className="p-8 menu-item font-serif" href="/event">
         Wedding Party
       </Link>
+      <Seperator />
+
       <Link
         id="home"
-        className="p-8 menu-item"
+        className="p-8 menu-item font-serif"
         href="https://www.crateandbarrel.com/gift-registry/adriana-dan-morton/r6551952"
       >
         Registry
       </Link>
+      <Seperator />
       <Link
         id="home"
-        className="p-8 menu-item"
+        className="p-8 menu-item font-serif"
         href="/invitation.pdf"
         download="Invitation"
       >
-        Invitation PDF
+        Invitation
       </Link>
     </Slide>
   );
