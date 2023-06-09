24daf401cc48ceb40fe4b3376eef59cf180b445e
Dan Morton <danmorton@Dans-Mac-mini.local>
2023-06-09 12:53:09 -0500

    Maintain mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
-import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
-import './globals.css'
+import type { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+import "./globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  //  return <Component {...pageProps} />
+  return (
+    <h1
+      style={{
+        position: "absolute",
+        left: "calc(50% - 150px)",
+        top: "40%",
+        width: 300,
+        fontSize: 26,
+      }}
+    >
+      This page has been taken down for contruction 👷
+    </h1>
+  );
 }
-
-export default MyApp
+export default MyApp;
